b67a04eab337378ebade550ffa67d0f01aed4332
Mally13 <mallymutuku13@gmail.com>
2023-07-26 10:55:33 +0300

    Fix: Memory issues

--- get_location.c
+++ get_location.c
@@ -7,24 +7,30 @@
 char *get_cmd_path(char *command)
 {
 	char *path, *path_duplicate, *path_token, *file_path;
+	char *current_dir_path;
 	int command_len, directory_len;
 	struct stat buffer;
 
+	if (!command || strlen(command) == 0)
+		return NULL;
 	path = getenv("PATH");
-
 	if (path)
 	{
 		path_duplicate = strdup(path);
-		command_len = strlen(command);
 		path_token = strtok(path_duplicate, ":");
 		while (path_token != NULL)
 		{
 			directory_len = strlen(path_token);
-			file_path = malloc(sizeof(char *) + (directory_len + command_len + 2));
+			command_len = strlen(command);
+			file_path = malloc(directory_len + command_len + 2);
+			if (!file_path)
+			{
+				free(path_duplicate);
+				return (NULL);
+			}
 			strcpy(file_path, path_token);
 			strcat(file_path, "/");
 			strcat(file_path, command);
-			strcat(file_path, "\0");
 
 			if (stat(file_path, &buffer) == 0)
 			{
@@ -38,11 +44,11 @@ char *get_cmd_path(char *command)
 			}
 		}
 		free(path_duplicate);
-		if (stat(command, &buffer) == 0)
-		{
-			return (command);
-		}
-		return (NULL);
+	}
+	if (stat(command, &buffer) == 0)
+	{
+		current_dir_path = strdup(command);
+		return (current_dir_path);
 	}
 	return (NULL);
 }
