8c088dd64a2c3cb503e666c94de09406402c6642
HinnMachache <bennyhinn253.bh@gmail.com>
2023-07-26 10:40:06 -0700

    simple shell

--- commandExec.c
+++ commandExec.c
@@ -14,9 +14,15 @@ void execute_cmd(char **argv)
 		command = argv[0];
 
 		if (strcmp(command, "exit") == 0)
+		{
 			handle_exit();
+			return;
+		}
 		if (strcmp(command, "env") == 0)
+		{
 			handle_env();
+			return;
+		}
 		absolute_command = get_cmd_path(command);
 		if (absolute_command && access(absolute_command, X_OK) == 0)
 		{
